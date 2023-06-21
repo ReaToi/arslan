11d57a00fcd9b84479023fc4f6eae32e661661a7
ReaToi <reatoi@mail.ru>
2023-06-21 08:58:19 +0600

    last commit

--- api/views.py
+++ api/views.py
@@ -1,12 +1,16 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import Category, Product
 from .serializers import ProductSerializer, CategorySerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 # Create your views here.
 
 
 class ProductAPIView(ListAPIView):
     queryset = Product.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ["category_id"]
+    search_fields = ["title", "description"]
     serializer_class = ProductSerializer
 
 
--- arslan/settings.py
+++ arslan/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_yasg",
     "api",
+    "django_filters"
 
 ]
 
